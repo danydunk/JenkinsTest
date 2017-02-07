e40375be7027674008bbce26b70257e2a7e77df1
Daniele <daniele.zuddas@usi.ch>
2017-02-07 09:18:22 +0100

    fixed

--- test/ex5/artist.form.js
+++ test/ex5/artist.form.js
@@ -19,7 +19,7 @@ module.exports = {
       .assert.visible('input[name=genre]')
       .assert.visible('input[name=artwork]')
       .assert.visible('input[name=dateCreated]')
-      .assert.value("input[name=genre]", "Black MMetal")
+      .assert.value("input[name=genre]", "Black Metal")
       .end();
   }
 };
